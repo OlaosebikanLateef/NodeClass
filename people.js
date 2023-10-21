42184c1d9c1504562f26cd96f64fc5c6101924c3
OlaosebikanLateef <laolaosebikan@gmail.com>
2023-10-21 21:39:29 +0100

    new

---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ['Lateef', 'Aishah', 'Idowu', 'Somod', 'Eniola'];
+const ages = [20, 30, 24, 34, 12]
+console.log(people);
+
+module.exports = {
+    people, ages
+};
---
+++ tut001.js
@@ -0,0 +1,15 @@
+// const name = "Lateef";
+// console.log(name);
+// const greet = (name) =>{
+//     console.log(`hello, ${name}`);
+// }
+// greet('Lateef');
+
+
+// console.log(global);
+
+global.setTimeout(() => {
+    console.log("Time out");
+}, 3000);
+
+
